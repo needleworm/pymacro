8c7246034be4e9b3ecbec2bcf273012ca1782f15
needleworm <bhban@kakao.com>
2020-03-02 17:46:42 +0900

    타이핑 성능 개선

--- pywinmacro.py
+++ pywinmacro.py
@@ -59,6 +59,13 @@ KEYMAP = {
 }
 
 
+# 대문자 특수문자를 위한 딕셔너리입니다.
+UPPER_SPECIAL = {
+    "!": 1,    "@": 2,    "#": 3,    "$": 4,    "%": 5,    "^": 6,
+    "&": 7,    "*": 8,    "(": 9,    ")": 0,    "~": '`',    "|": '\\'
+}
+
+
 # 마우스를 특정위치로 이동시키는 함수
 def move_mouse(location):
     # location 을 입력받아 이 위치로 마우스를 이동시킵니다.
@@ -106,6 +113,7 @@ def key_press_once(key):
 
 
 # 글자 입력 (클립보드에 복사 후 붙여넣기)
+# 한글일 경우에만 사용하세요. 한글은 형태소 분해가 곤란하여 그렇습니다.
 def type_in(string):
     # 클립보드에 스트링을 집어넣습니다.
     os.system('echo ' + string + '| clip')
@@ -117,6 +125,18 @@ def type_in(string):
     key_off("control")
     key_off("v")
 
+
+# 영어, 숫자, 특수문자로 된 스트링을 바로 입력하는 함수입니다.
+def typinrg(string):
+    for el in string.strip():
+        if el.isupper():
+            key_on("shift")
+            key_press_once("el.lower")
+            key_off("shift")
+        elif el in UPPER_SPECIAL:
+            key_press_once(UPPER_SPECIAL[el])
+        else:
+            key_press_once("el")
 
 # 키를 계속 누르고 있도록 하는 함수입니다.
 def key_on(key):
